f94782546bfc6e61e0093eac0ecf814cd82d5919
admin <admin@lightserve.private>
2011-09-01 16:03:36 +0100

    Added custom module files

---
+++ wysiwyg_styles.js
@@ -0,0 +1,14 @@
+CKEDITOR.addStylesSet('my_styles',
+    [
+      {
+        name: 'Image caption',
+        element: 'img',
+        attributes: {'class' : 'caption'} 
+      },
+      {
+        name: 'Spaced OL list',
+        element: 'ol',
+        attributes: {'class' : 'spaced'}
+      }
+    ]
+    );
